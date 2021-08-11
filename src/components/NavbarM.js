65a6d42f46cc43b09ba3b4975d3b7e40cd838662
galihif <galihindra2401@mail.ugm.ac.id>
2021-08-11 11:43:44 +0700

    ganti navbar ketika user login

--- src/components/NavbarM.js
+++ src/components/NavbarM.js
@@ -1,6 +1,8 @@
 //Library
 import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch, connect } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import Gravatar from 'react-gravatar'
 
 
 //Styles
@@ -8,22 +10,90 @@ import {
     Container,
     Navbar,
     Nav,
-    Button
+    Button,
+    Row,
+    Col
 } from 'react-bootstrap';
+import API from '../config/API';
+import './NavbarM.scss'
 
 // //Images
 // import navbar_logo from '../assets/navbar_logo.png'
 
-const NavbarM = () => {
-    const state = useSelector((state) => state)
-
+const NavbarM = (props) => {
     //State
-    const [isLogged, setLogged] = useState(false)
+    const state = useSelector((state) => state)
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const userToken = state.userToken
+    const accessToken = userToken.access
+    const refreshToken = userToken.refresh
+    const [isLogged, setLogged] = useState(state.isLogged)
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [role, setRole] = useState("")
+    const dashboardPath = role === 'DONATUR' ? "/dashboarddonor" : "/dashboardfundraiser"
+
+    const headers = {
+        Accept: "application/json",
+        Authorization: `Bearer ${accessToken}`
+    }
+
 
     //Method
     useEffect(() => {
         
     }, [isLogged])
+
+    const getUserData = () => {
+        API.getCurrentUser(headers)
+            .then((res) => {
+                const userData = res.data
+                setFirstName(userData.first_name)
+                setLastName(userData.last_name)
+                setEmail(userData.email)
+                setRole(userData.role)
+                setLogged(true)
+            })
+            .catch((err) => {
+                console.log(err)
+                setLogged(false)
+                refreshUserToken()
+            })
+    }
+
+    const refreshUserToken = () => {
+        const body = {
+            refresh: refreshToken
+        }
+        API.refresh(body)
+            .then((res) => {
+                console.log(res.data)
+                dispatch({ type: 'REFRESH', userToken: res.data })
+                setLogged(true)
+            })
+            .catch((err) => {
+                setLogged(false)
+            })
+    }
+
+    if (state.isLogged) {
+        getUserData()
+    }
+
+    const navLogged = 
+    <Row>
+        <Col className="d-flex nav-user" onClick={() => history.push(dashboardPath)}>
+            <Gravatar email={email} size="30" style={{ borderRadius: "20em" }} />
+            <p className="my-0 ms-2 fs-5" >{firstName} {lastName}</p>
+        </Col>
+    </Row>
+    const navNotLogged = 
+    <div>
+        <Button variant="outline-primary mx-2" href="/login">Login</Button>
+        <Button variant="primary" href="registerdonor" >Register</Button>
+    </div>
 
     return (
         <div>
@@ -36,8 +106,9 @@ const NavbarM = () => {
                             <Nav.Link href="/">Home</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Button variant="outline-primary mx-2" href="/login">Login</Button>
-                            <Button variant="primary" href="registerdonor" >Register</Button>
+                            {
+                                isLogged ? navLogged : navNotLogged
+                            }
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
