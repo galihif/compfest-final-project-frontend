44f5ec84fa5aa8c5346e7885d0a3a7dc032a3143
galihif <galihindra2401@mail.ugm.ac.id>
2021-08-12 21:25:58 +0700

    add props to get data

--- src/components/CampaignCard.js
+++ src/components/CampaignCard.js
@@ -11,21 +11,22 @@ import {
     Card
 } from 'react-bootstrap';
 
-const CampaignCard = () => {
+const CampaignCard = (props) => {
     const history = useHistory()
+    const percentage = (props.amount / props.targetAmount) * 100
     return(
         <Card className="campaign-card" style={{ width: "20em" }} onClick={() => history.push('/campaign')} >
             <Card.Img variant="top" src="https://www.islamic-relief.org/wp-content/uploads/2021/04/original--1024x683.jpg" style={{ height: "10em", objectFit: "cover" }} />
             <Card.Body>
-                <Card.Title>Help Poor People in India</Card.Title>
-                <ProgressBar now={10} style={{ height: "0.8em" }} />
+                <Card.Title>{props.title}</Card.Title>
+                <ProgressBar now={percentage} style={{ height: "0.8em" }} />
                 <Row>
                     <Col lg={6}>
-                        <p className="text-start m-0" style={{ fontSize: "12px" }}>Rp. 10.000.000</p>
+                        <p className="text-start m-0" style={{ fontSize: "12px" }}>Rp. {props.amount}</p>
                         <p className="text-start fw-bold" style={{ fontSize: "12px" }}>Raised</p>
                     </Col>
                     <Col lg={6}>
-                        <p className="text-end m-0" style={{fontSize:"12px"}}>Rp.100.000.000</p>
+                        <p className="text-end m-0" style={{ fontSize: "12px" }}>Rp.{ props.targetAmount}</p>
                         <p className="text-end fw-bold" style={{fontSize:"12px"}}>Target</p>
                     </Col>
                 </Row>
