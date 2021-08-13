25b8f8ca54b47ff88e498571f520c822d88bebab
novel <novelbafagih@student.ub.ac.id>
2021-08-13 11:41:01 +0700

    finshing unit test of AdminFundraiserCard

--- src/components/Admin/test/AdminFundraiserCard.test.js
+++ src/components/Admin/test/AdminFundraiserCard.test.js
@@ -2,30 +2,48 @@
 
 import React from "react";
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure} from "enzyme";
+import {shallow, configure} from "enzyme";
 import AdminFundraiserCard from "../AdminFundraiserCard";
 import Gravatar from "react-gravatar";
-import { Button,Card } from "react-bootstrap";
+import {Button, Card} from "react-bootstrap";
 
 configure({adapter: new Adapter()});
 describe("<AdminFundraserCard />", () => {
+	const name = "jokowiddod";
+	const email = "jokowiddo@gmail.com";
+	const description = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used demonstrate the visual ...";
 	const card = shallow(
 		<AdminFundraiserCard
-			title="jokowiddod"
-			email="jokowiddo@gmail.com"
-			description="In publishing and graphic design, Lorem ipsum is a placeholder text commonly used demonstrate the visual ..."
+			title={name}
+			email={email}
+			description={description}
 		/>
 	);
+
 	it('have a gravatar',() => {
 		expect(card.find(Gravatar)).toBeTruthy();
 	});
+
 	it('have 3 buttons',() => {
+		expect(card.find(Button)).toBeTruthy();
 		expect(card.find(Button)).toHaveLength(3);
 	});
+
 	it('is a card',() => {
 		expect(card.find(Card)).toBeTruthy();
 		expect(card.find(Card.Body)).toBeTruthy();
 		expect(card.find(Card.Title)).toBeTruthy();
 	});
 
+	it('contains name',() => {
+		expect(card.text()).toMatch(new RegExp(name));
+	});
+
+	it('contains email',() => {
+		expect(card.text()).toMatch(new RegExp(email));
+	});
+
+	it('contains description',() => {
+		expect(card.text()).toMatch(new RegExp(description));
+	});
 });
