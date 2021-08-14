3ad61936bfcd8716efcd04f0aec2402033b3d5af
galihif <galihindra2401@mail.ugm.ac.id>
2021-08-14 21:49:51 +0700

    refactoring navigation route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 //Library
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
 } from "react-router-dom";
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -13,16 +11,8 @@ import { store, persistor } from './config/Redux/store'
 //Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//component
-import Home from './pages/Home'
-import NavbarM from "./components/Navbar/NavbarM";
-import Footer from "./components/Footer/Footer";
-import RegisterDonor from "./pages/RegisterDonor";
-import RegisterFundraiser from "./pages/RegisterFundraiser";
-import Login from "./pages/Login";
-import CampaignDetails from "./pages/CampaignDetails";
-import DashboardDonor from "./pages/DashboardDonor";
-import DashboardFundraiser from "./pages/DashboardFundraiser";
+//Routing
+import { RouterConfig } from "./navigation/RouterConfig";
 
 
 
@@ -31,33 +21,7 @@ function App() {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Router>
-          <NavbarM />
-          <div>
-            <Switch>
-              <Route path="/dashboardfundraiser">
-                <DashboardFundraiser/>
-              </Route>
-              <Route path="/dashboarddonor">
-                <DashboardDonor/>
-              </Route>
-              <Route path="/campaign/:id">
-                <CampaignDetails/>
-              </Route>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/registerdonor">
-                <RegisterDonor />
-              </Route>
-              <Route path="/registerfundraiser">
-                <RegisterFundraiser />
-              </Route>
-              <Route exact path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-          <Footer />
+          <RouterConfig/>
         </Router>
       </PersistGate>
     </Provider>
---
+++ src/navigation/CONSTANTS.js
@@ -0,0 +1,7 @@
+export const ROOT = "/";
+export const LOGIN = "/login";
+export const REGFUND = "/registerfundraiser";
+export const REGDONOR = "/registerdonor";
+export const CAMPAIGN = "/campaign/:id";
+export const DASHFUND = "/dashboardfundraiser";
+export const DASHDONOR = "/dashboarddonor";
