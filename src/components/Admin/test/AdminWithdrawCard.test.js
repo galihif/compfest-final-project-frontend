8b09cb5dbc300f3ff61f80287bbf8b0152119878
novel <novelbafagih@student.ub.ac.id>
2021-08-15 21:53:36 +0700

    added withdrawCard test

--- src/components/Admin/test/AdminWithdrawCard.test.js
+++ src/components/Admin/test/AdminWithdrawCard.test.js
@@ -3,31 +3,31 @@
 import React from "react";
 import Adapter from 'enzyme-adapter-react-16';
 import {shallow, configure} from "enzyme";
-import {Card} from "react-bootstrap";
 
-// Component
 import Gravatar from "react-gravatar";
-import AdminCampaignProposalCard from "../AdminCampaignProposalCard";
+import {Card,Button} from "react-bootstrap";
+import AdminWithdrawCard from "../AdminWithdrawCard";
 
 configure({adapter: new Adapter()});
 describe("<AdminFundraserCard />", () => {
-	const title = "saving live";
 	const name = "jokowiddod";
 	const email = "jokowiddo@gmail.com";
-	const target = 100000;
+	const bankName = "BCA"
+	const amount=10000000;
+	const accountNumber= "1293048231";
 
 	const component = 
-		<AdminCampaignProposalCard 
-			title={title}
+		<AdminWithdrawCard
 			name={name}
 			email={email}
-			target={target}
+			amount={amount}
+			bankName={bankName}
+			accountNumber={accountNumber}
 		/>
-
 	const card = shallow(
 		component
 	);
-	
+
 	it('have a gravatar',() => {
 		expect(card.find(Gravatar)).toBeTruthy();
 	});
@@ -38,12 +38,19 @@ describe("<AdminFundraserCard />", () => {
 		expect(card.find(Card.Title)).toBeTruthy();
 	});
 
-	it('contains props', () => {
-		expect(card.text()).toMatch(new RegExp(title));
+	it('contains name',() => {
 		expect(card.text()).toMatch(new RegExp(name));
-		expect(card.text()).toMatch(new RegExp(email));
-		expect(card.text()).toMatch(new RegExp(target));
 	});
 
+	it('contains 2 cardrow',() => {
+		expect(card.find(Button)).toHaveLength(2);
+	});
+
+	it('contains props', () => {
+		expect(card.text()).toMatch(new RegExp(name));
+		expect(card.text()).toMatch(new RegExp(amount.toString()));
+		expect(card.text()).toMatch(new RegExp(bankName));
+		expect(card.text()).toMatch(new RegExp(accountNumber));
+	});
 
 });
