9a024ef1230be0da3e5cf2e6df8f4c90e52b33fd
novel <novelbafagih@student.ub.ac.id>
2021-08-16 09:51:03 +0700

    deleting comment in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,11 @@
 
-//Component
-// import FeaturedCard from "../components/FeaturedCard"
 import Header from "../components/Header/Header"
-// import CampaignCard from "../components/CampaignCard"
 import FeaturedCampaign from "../containers/FeaturedCampaign"
-// import AdminFundraiserCard from "../components/AdminFundraiserCard"
-// import AdminWithdrawCard from "../components/AdminWithdrawCard"
-// import AdminUserCard from "../components/AdminUserPaymentCard"
 
 const Home = () => {
     return (
         <div>
             <Header/>
-            {/* <FeaturedCard title={'Featured Campaigns'}>
-                <CampaignCard/>
-            </FeaturedCard> */}
             <FeaturedCampaign/>
         </div>
     )
