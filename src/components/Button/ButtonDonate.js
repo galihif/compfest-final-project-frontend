41708f8c3952f5564e80e091509da06a39cba03b
galihif <galihindra2401@mail.ugm.ac.id>
2021-08-16 20:50:45 +0700

    refactor campaign details

---
+++ src/components/Button/ButtonDonate.js
@@ -0,0 +1,108 @@
+//Library
+import React, { useState, useEffect, useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useSelector, useDispatch, connect } from 'react-redux'
+import { Button, Modal, Form, Row } from 'react-bootstrap'
+import API from '../../config/API'
+
+const ButtonDonate = (props) => {
+
+    const history = useHistory()
+
+    const userToken = props.userToken
+    const accessToken = userToken.access
+    const refreshToken = userToken.refresh
+
+    const [showDonate, setShowDonate] = useState(false)    
+    const [userRole, setUserRole] = useState(props.userData.role)
+    const [walletAmount, setWalletAmount] = useState(props.userData.wallet_amount)
+    const [donateAmount, setDonateAmount] = useState(0)
+    const [password, setPassword] = useState("")
+    const [toWithdrawAmount, setToWithdrawAmount] = useState(0)
+
+
+    const toggleDialog = () => setShowDonate(!showDonate)
+
+    const headers = {
+        Accept: "application/json",
+        Authorization: `Bearer ${accessToken}`
+    }
+
+    const handleChange = (e) => {
+        switch (e.target.id) {
+            case "donateAmount":
+                setDonateAmount(e.target.value)
+                break
+            case "password":
+                setPassword(e.target.value)
+                break
+            default:
+                break
+        }
+    }
+    
+    const handleClickPay = () => {
+        const body = {
+            amount: parseInt(donateAmount),
+            password: password
+        }
+        if (donateAmount <= walletAmount && donateAmount >= 5000) {
+            API.donateCampaignById(props.id, body, headers)
+                .then((res) => {
+                    console.log(res)
+                    toggleDialog()
+                    alert(res.data.status)
+                    window.location.reload()
+                })
+                .catch((err) => {
+                    console.log(err.response)
+                    alert(err.response.data)
+                })
+        } else if (walletAmount <= donateAmount) {
+            alert("Your E-wallet balance is not enough")
+        } else if (parseInt(donateAmount) < 5000) {
+            alert("Minimum amount is Rp 5000")
+        }
+    }
+
+    return (
+        <Row className="d-grid my-2">
+            <Button variant="primary" type="submit" onClick={() => toggleDialog()}>
+                DONATE
+            </Button>
+            <Modal show={showDonate}>
+                <Modal.Header >
+                    <Modal.Title>Donate To This Campaign</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-3" controlId="donateAmount" onChange={handleChange}>
+                        <Form.Label>Amount</Form.Label>
+                        <Form.Control type="number" placeholder="Enter Amount (Rp)" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="password" onChange={handleChange}>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter Your Password" />
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={toggleDialog}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={handleClickPay}>
+                        Pay with E-Wallet
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </Row>
+    )
+
+}
+
+function mapStateToProps(state, ownProps) {
+    return {
+        isLogged: state.isLogged,
+        userData: state.userData,
+        userToken: state.userToken
+    };
+}
+export default connect(mapStateToProps)(ButtonDonate)
---
+++ src/components/Button/ButtonWithdrawStop.js
@@ -0,0 +1,173 @@
+//Library
+import React, { useState, useEffect, useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useSelector, useDispatch, connect } from 'react-redux'
+import { Button, Modal, Form, Row } from 'react-bootstrap'
+import API from '../../config/API'
+
+const ButtonWithdrawStop = (props) => {
+
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    const userToken = props.userToken
+    const accessToken = userToken.access
+    const refreshToken = userToken.refresh
+
+    const [showWithdraw, setShowWithdraw] = useState(false)
+    const [showStop, setShowStop] = useState(false)
+
+    const [campaign, setCampaign] = useState(props.campaign)
+    
+    const [loading, setLoading] = useState(false)
+    const [toWithdrawAmount, setToWithdrawAmount] = useState(0)
+    const remainingAmount = campaign.amount - campaign.withdraw_amount
+
+    const toggleDialogWithdraw = () => setShowWithdraw(!showWithdraw)
+    const toggleDialogStop = () => setShowStop(!showStop)
+
+    const headers = {
+        Accept: "application/json",
+        Authorization: `Bearer ${accessToken}`
+    }
+
+    const handleChange = (e) => {
+        switch (e.target.id) {
+            case "toWithdrawAmount":
+                setToWithdrawAmount(e.target.value)
+                break
+            default:
+                break
+        }
+    }
+
+    const handleStopCampaign = () => {
+        setLoading(true)
+        API.stopCampaignById(campaign.id, headers)
+            .then((res) => {
+                console.log(res.data)
+                setLoading(false)
+                toggleDialogStop()
+            })
+            .catch((err)=> {
+                if (err.response.status === 401){
+                    // refreshUserToken(false)
+                }
+                console.log(err)
+            })
+    }
+    
+    const handleClickWithdraw = () => {
+        setLoading(true)
+        const body = {
+            amount: parseInt(toWithdrawAmount)
+        }
+        if (toWithdrawAmount <= remainingAmount && toWithdrawAmount !== 0) {
+            API.withdrawCampaignById(campaign.id, body, headers)
+                .then((res) => {
+                    alert("Withdraw Requested")
+                    setLoading(false)
+                    // history.push('/dashboardfundraiser')
+                    toggleDialogWithdraw()
+                })
+                .catch((err) => {
+                    if(err.response.status === 401){
+                        refreshUserToken(true)
+                    } else {
+                        setLoading(false)
+                    }
+                })
+        } else if (toWithdrawAmount > remainingAmount) {
+            alert("amount is too high")
+        } else if (toWithdrawAmount === "undefined") {
+            alert("Fill the amount")
+        }
+    }
+
+    const refreshUserToken = (isWithdraw) => {
+        const body = {
+            refresh: refreshToken
+        }
+        API.refresh(body)
+            .then((res) => {
+                console.log(res.data)
+                dispatch({ type: 'REFRESH', userToken: res.data })
+                if(isWithdraw){
+                    handleClickWithdraw()
+                } else {
+                    // handleStopCampaign()
+                }
+            })
+            .catch((err) => {
+                console.log(err, "ref")
+            })
+    }
+
+    return (
+        <div>
+            <Row className="d-grid">
+                <Button variant="primary" type="submit" onClick={toggleDialogWithdraw}>
+                    WITHDRAW
+                </Button>
+            </Row>
+            <Row className="d-grid">
+                <Button variant="outline-danger my-3" type="submit" onClick={toggleDialogStop}>
+                    STOP THIS CAMPAIGN
+                </Button>
+            </Row>
+            <div>
+                <Modal show={showWithdraw}>
+                    <Modal.Header >
+                        <Modal.Title>Withdraw This Campaign</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form.Group className="mb-3" controlId="toWithdrawAmount" onChange={handleChange}>
+                            <Form.Label>Amount</Form.Label>
+                            <Form.Control type="number" placeholder="Enter Amount (Rp)" />
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={toggleDialogWithdraw}>
+                            Cancel
+                        </Button>
+                        <Button variant="primary" onClick={handleClickWithdraw} disable={loading} >
+                            {
+                                loading ? <div>Loading...</div> : <div>Make Request</div>
+                            }
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <Modal show={showStop}>
+                    <Modal.Header >
+                        <Modal.Title>Finish This Campaign</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form.Text>Are you sure want to stop this campaign?</Form.Text>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={toggleDialogStop}>
+                            Cancel
+                        </Button>
+                        <Button variant="danger" onClick={handleStopCampaign} disable={loading} >
+                            {
+                                loading ? <div>Loading...</div> : <div>Stop</div>
+                            }
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+            </div>
+        </div>
+    )
+
+}
+
+function mapStateToProps(state, ownProps) {
+    return {
+        isLogged: state.isLogged,
+        userData: state.userData,
+        userToken: state.userToken
+    };
+}
+export default connect(mapStateToProps)(ButtonWithdrawStop)
