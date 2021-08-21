049fdcf0a3e38e39364a789a5b19124ac3a786fa
novel <novelbafagih@student.ub.ac.id>
2021-08-21 14:20:28 +0700

    adding route to admin home page

--- src/components/Header/AdminHeader.js
+++ src/components/Header/AdminHeader.js
@@ -14,9 +14,17 @@ import {
 
 //Assets
 import headerImage from '../../assets/adminHeaderImage.jpg'
+import { useHistory } from 'react-router-dom';
+
 
 
 const Header = () => {
+    const history = useHistory();
+    const routeHandler = (endUrl) => {
+        history.push(`/admin/${endUrl}`);
+        window.scrollTo(0, 700)
+
+    }
     return (
         <Container className="header-container m-0" fluid>
             <Row className="p-5">
@@ -24,6 +32,21 @@ const Header = () => {
                     <div>
                         <h3 className="">Help Others</h3>
                         <p>double check to verified/rejecting something</p>
+                        <p>Where do you want to go?</p>
+    
+                        <Col lg={12} className="d-flex align-items-center">
+                            <Button variant="outline-primary" className='w-100' onClick={() => routeHandler('campaign')}>Campaign Proposal</Button>
+                        </Col>
+                        <Col lg={12} className="d-flex align-items-center mt-2" >
+                            <Button variant="outline-primary" className='w-100' onClick={() => routeHandler('topup')}>User Topup </Button>
+                        </Col>
+                        <Col lg={12} className="d-flex align-items-center mt-2" >
+                            <Button variant="outline-primary"className='w-100' onClick={() => routeHandler('withdraw')}>Fundraiser Withdraw </Button>
+                        </Col>
+                        <Col lg={12} className="d-flex align-items-center mt-2" >
+                            <Button variant="outline-primary"className='w-100' onClick={() => routeHandler('fundraiser')}>Fundraiser Proposal </Button>
+                        </Col>
+                    
                     </div>
                 </Col>
                 <Col lg={6} md={4}>
