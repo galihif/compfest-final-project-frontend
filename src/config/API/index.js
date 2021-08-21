09ca1087383f572fac7425ca761e4e4bf3af06e7
novel <novelbafagih@student.ub.ac.id>
2021-08-21 07:50:09 +0700

    empty

--- src/config/API/index.js
+++ src/config/API/index.js
@@ -18,11 +18,23 @@ const getUserTopUpList = (headers) => Get('topup', headers)
 const getUserCampaignList = (headers) => Get('fundraiser/campaigns', headers)
 const getWithdrawRequestList = (headers) => Get('withdraw', headers)
 const getAllCampaign = (headers) => Get('campaigns', headers)
+const getListFundraiser = (headers) => Get('admin/fundraiser-requests', headers)
+const getListCampaignProposal = (headers) => Get('admin/proposals', headers)
+const getUserPayments = (headers) => Get('topup/requests', headers)
+const getListWithdrawsAdmin = (headers) => Get('withdraw/requests', headers)
+
 // const getCampaignProposal = () => Get('admin/proposals')
 const getDonateHistoryDonor = (headers) => Get('donate',headers)
 const getCampaignByIdFundraiser = (id, headers) => Get(`fundraiser/campaigns/${id}`,headers)
 const getCampaignByIdDonor = (id, headers) => Get(`donor/campaigns/${id}`,headers)
+const getFundraiserByIdAdmin = (id, headers) => Get(`admin/fundraiser-requests/${id}`,headers)
+const getCampaignProposalById = (id, headers) => Get(`admin/proposals/${id}`,headers)
 
+// PUT
+const putAcceptFundraiser = (body, headers) => Put(`admin/fundraiser-requests`,body,headers);
+const putAcceptCampaign = (id,body, headers) => Put(`admin/proposals/${id}`,body,headers);
+const putAcceptUserPayment = (body,headers) => Put('topup/requests',body, headers)
+const putAcceptWithdraw = (body,headers) => Put('withdraw/requests',body, headers)
 
 
 // DELETE
@@ -37,8 +49,15 @@ const API = {
     getAllCampaign,
     getCampaignByIdFundraiser,
     getCampaignByIdDonor,
+    getListFundraiser,
+    getFundraiserByIdAdmin,
+    getListCampaignProposal,
+    getCampaignProposalById,
+    getUserPayments,
     // getCampaignProposal,
     getDonateHistoryDonor,
+    getListWithdrawsAdmin,
+    putAcceptWithdraw,
     topUp,
     getUserTopUpList,
     getWithdrawRequestList,
@@ -46,7 +65,9 @@ const API = {
     donateCampaignById,
     withdrawCampaignById,
     deleteCampaignById,
+    putAcceptFundraiser,
+    putAcceptCampaign,
+    putAcceptUserPayment
 }
 
 export default API;
-
